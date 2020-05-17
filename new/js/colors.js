17a85696abe229f9c38820e41730b1ac88d354f1
Garrison Davis <garrison@gdavis.io>
2020-05-17 01:29:10 -0400

    Properly convert the colors between RGB and HWB to get the shuffle to work

--- new/js/colors.js
+++ new/js/colors.js
@@ -2,7 +2,7 @@
 
 var colors = {};
 
-colors.defaultDuration = 1.5;
+colors.defaultDuration = -1;//1.5;
 colors.frameLength = 1/30;
 
 var root = document.documentElement;
@@ -26,25 +26,29 @@ function HWBColor(inputHex)
         
         var maxIdx = rgb.indexOf(max);
         //ie max r : h = (g - b) / range * 60 + 0
-        this.hwb[0] = (rgb[(maxIdx+1)%3] - rgb[(maxIdx+2)%3]) / (max - min) * 60 + 120 * maxIdx;
-        this.hwb[0] = (this.hwb[0] + 360) % 360; //ensure positive
+        this.hwb[0] = (rgb[(maxIdx+1)%3] - rgb[(maxIdx+2)%3]) / (max - min) + 2 * maxIdx;
+        this.hwb[0] = (this.hwb[0] + 6) % 6; //ensure positive
         this.hwb[1] = min;
         this.hwb[2] = 255 - max;
     }
     //https://github.com/Qix-/color/blob/master/index.js
     this.Rotate = function(deg)
     {
-        this.hwb[0] = (this.hwb[0] + deg + 360) % 360;
+        this.hwb[0] = (this.hwb[0] + deg/360 + 6) % 6;
     }
     //http://alvyray.com/Papers/CG/hwb2rgb.htm
     this.ToRgb = function()
     {
-        var v = Math.round(255-this.hwb[2]);
-        var i = Math.floor(this.hwb[0]/60);
-        var f = this.hwb[0]/60 - i;
-        if(!(i % 1)) f = 1 - f;
+        var v = 255-this.hwb[2];
+        var i = Math.floor(this.hwb[0]);
+        var f = this.hwb[0] - i;
         var w = this.hwb[1];
-        var n = this.hwb[2] + Math.round(f * (v - w));
+        var n;
+        if(i&1){
+            //can maybe do something like f = 1 - f; to be more efficient
+            n = (1-f)*v + f * w;
+        } else
+            n = (1-f)*w + f * v;
         switch(i){
             case 6: 
             case 0: return [v, n, w];
@@ -53,40 +57,45 @@ function HWBColor(inputHex)
             case 3: return [w, n, v];
             case 4: return [n, w, v];
             case 5: return [v, w, n];
+            default: return null;
         }
     }
     this.ToHex = function()
     {
         var rgb = this.ToRgb();
-        
-        return '#'+(rgb[2]+rgb[1]*256+rgb[0]*65536).toString(16);
+        var pre = (Math.round(rgb[2])+Math.round(rgb[1])*256+Math.round(rgb[0])*65536).toString(16);
+        while(pre.length < 6)
+        pre= '0'+pre;
+        return '#'+pre;
     }
     this.FromHex(inputHex);
 }
 
-colors.Shuffle = function(duration, direction)
+colors.Shuffle = function(duration, speed)
 {
     //clear existing method
     if(colors.timeout)
         clearTimeout(colors.timeout)
     
     //choose a psuedo-random direction
-    if(!direction)
-        direction = Math.random();
+    //if(!direction)
+    //    direction = Math.random();
     
     var letter = 'A';
     for(var i in colors.colors)
     {
         var color = colors.colors[i];
-        color.Rotate(3);
+        color.Rotate(speed);
         var hex = color.ToHex();
+        if(letter=='A')
+            console.log(color.hwb);
         root.style.setProperty('--color'+letter, hex);
         letter = increment(letter);
     }
     
     //apply the rotation
-    if(duration > colors.frameLength)
-    colors.timeout = setTimeout(colors.Shuffle, colors.frameLength*1000, duration - colors.frameLength);
+    if(duration< 0 || duration > colors.frameLength)
+    colors.timeout = setTimeout(colors.Shuffle, colors.frameLength*1000, duration - colors.frameLength, speed);
     //else
         //colors.timeout = 0 or something
 }
@@ -106,4 +115,4 @@ colors.Cache = function()
     
 colors.Cache();
                  
-colors.timeout = colors.Shuffle(colors.defaultDuration);
+colors.timeout = colors.Shuffle(colors.defaultDuration, 3);
